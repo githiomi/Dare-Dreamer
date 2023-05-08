2f284d1598d7f7311641ecb6ecb1d5d6edbc3071
Daniel Githiomi <danielgithiomi@gmail.com>
2023-05-08 11:35:03 +0400

    Refactoed index page styling for responsiveness

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,7 @@
-// Navbar toggle
-const hamburger = document.querySelector('.hamburger i');
-const navigation = document.querySelector('.navigation');
-
-hamburger.addEventListener('click', () => {
-
-    hamburger.classList.toggle('bx-x');
-    navigation.classList.toggle('show');
-
-});
-
 // Parallax Effect and Navbar Trigger
 const moon = document.querySelector('.moon');
 const leftCloud = document.querySelector('.cloud1');
 const rightCloud = document.querySelector('.cloud2');
-const logo = document.querySelector('.logo');
 
 window.addEventListener('scroll', () => {
 
@@ -23,39 +11,10 @@ window.addEventListener('scroll', () => {
         leftCloud.style.left = value * -1 + 'px';
         rightCloud.style.right = value * -1 + 'px';
 
-        logo.style.width = '5rem';
         moon.classList.add('toLeft');
     }else{
-        logo.style.width = '6rem';
         moon.classList.remove('toLeft');
     }
-
-    // Add a sticky navigation bar
-    const header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // To remove navbar icon on select and scroll
-    hamburger.classList.remove('bx-x');
-    navigation.classList.remove('show');
-
-    // Change navbar active link
-    sections.forEach(section => {
-        let top = window.scrollY;
-
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let sectionId = section.getAttribute('id');
-
-        if (top >= offset && top < (offset + height)) {
-
-            links.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector('.navigation li a[href*=' + sectionId + ']').classList.add('active');
-            });
-
-        };
-    });
 
 })
 
